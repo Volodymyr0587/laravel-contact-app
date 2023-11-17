753d09a41f2d587c36af7e445a6a9aebf3bc84d4
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-11-17 11:32:01 +0200

    Display records from DB in table

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -9,9 +9,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    @foreach ($people as $person)
-                        {{ $person->firstname }}
-                    @endforeach
+
+                    <div class="flex items-center justify-end">
+                        <a class="bg-blue-600 text-white py-2 px-3 rounded-full hover:bg-yellow-300 hover:text-blue-600" href="{{ route('person.create') }}">Add Person</a>
+                    </div>
+
+                    <table class="table-fixed border-separate border-spacing-6">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($people as $person)
+                                <tr>
+                                    <td>{{ $person->firstname }} {{ $person->lastname }}</td>
+                                    <td>{{ $person->email }}</td>
+                                    <td>{{ $person->phone }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
