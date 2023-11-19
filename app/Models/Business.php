3409ea37055790d7a3a6ae370a9616a7fa7dfbad
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-11-19 13:11:36 +0200

    One To Many Relationship

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Business extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function people(): HasMany
+    {
+        return $this->hasMany(Person::class);
+    }
 }
--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Person extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function business(): BelongsTo
+    {
+        return $this->belongsTo(Business::class);
+    }
 }
