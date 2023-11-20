1d7d69d94846ecca72f18b4be05dd47a54aa29dd
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-11-20 16:19:33 +0200

    Define Many To Many relationship in models Business and BusinessCategory

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -24,5 +25,10 @@ class Business extends Model
     public function people(): HasMany
     {
         return $this->hasMany(Person::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(BusinessCategory::class, 'category_has_business');
     }
 }
--- app/Models/BusinessCategory.php
+++ app/Models/BusinessCategory.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class BusinessCategory extends Model
 {
     use HasFactory;
+
+    public function business(): BelongsToMany
+    {
+        return $this->belongsToMany(Business::class, 'category_has_business');
+    }
 }
