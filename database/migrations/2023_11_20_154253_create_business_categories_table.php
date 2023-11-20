f64a1923707d5365d1a1eb729d490281a16569db
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-11-20 15:53:00 +0200

    Create model BusinessCategory with migration; Create tables 'business_categories' and 'category_has_business' (pivot table)

---
+++ database/migrations/2023_11_20_154253_create_business_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category_name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        //? Pivot table between businesses and categories
+        Schema::create('category_has_business', function (Blueprint $table) {
+            $table->id();
+            $table->integer('business_category_id');
+            $table->integer('business_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business_categories');
+    }
+};
