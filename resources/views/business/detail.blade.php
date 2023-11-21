8dc871dba070f4134f8052666044235855634cc1
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-11-21 15:07:29 +0200

    Display business tasks

--- resources/views/business/detail.blade.php
+++ resources/views/business/detail.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Business') }} | {{ $business->name }}
+            {{ __('Business') }} | {{ $business->business_name }}
         </h2>
     </x-slot>
 
@@ -11,11 +11,11 @@
                 <div class="p-6 text-gray-900">
                     <div class="grid grid-cols-1 sm:grid-cols-6 gap-x-6 gap-y-6">
                         <div class="sm:col-span-3">
-                            <h3 class="font-semibold text-l pb-5">Business Details</h3>
+                            <h3 class="font-semibold text-l pb-5">Company Details</h3>
                             <dl>
-                                <dt class="font-semibold">Business Name</dt>
+                                <dt class="font-semibold">Name</dt>
                                 <dd class="pl-3">{{ $business->business_name }}</dd>
-                                <dt class="font-semibold">Email</dt>
+                                <dt class="font-semibold">Contact Email</dt>
                                 <dd class="pl-3">{{ $business->contact_email }}</dd>
                             </dl>
 
@@ -25,6 +25,10 @@
                         </div>
                         <div class="sm:col-span-3">
                             <h3 class="font-semibold text-l pb-5">Tasks</h3>
+                            @foreach ($business->tasks as $task)
+                                <h4 class="font-semibold">{{ $task->title }}</h4>
+                                <p>{{ $task->description }}</p>
+                            @endforeach
                         </div>
                     </div>
                 </div>
