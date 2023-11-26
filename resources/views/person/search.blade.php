e7a4fcb8ee4af4708e049d95ecf672408c34c08c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-11-26 18:33:13 +0200

    Add link to prev page

--- resources/views/person/search.blade.php
+++ resources/views/person/search.blade.php
@@ -1,6 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight inline-flex">
+            <a class="flex justify-left items-center mr-2" href="{{ URL::previous() }}">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                </svg>
+            </a>
             {{ __('People. Search result ror ') }} <span class="italic">"{{ $search }}"</span>
         </h2>
     </x-slot>
