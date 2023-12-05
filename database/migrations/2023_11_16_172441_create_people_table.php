85977a2dde2658347eda1cb3c9af7f49e9517949
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-05 21:44:15 +0200

    Change migrations for people and notes tables

--- database/migrations/2023_11_16_172441_create_people_table.php
+++ database/migrations/2023_11_16_172441_create_people_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->nullable();
