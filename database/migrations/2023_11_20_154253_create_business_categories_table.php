b0c44927f3ae94bb0284600ee75d0d47299edace
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-07 15:39:27 +0200

    Add foreinId user_id to migration business_categories table

--- database/migrations/2023_11_20_154253_create_business_categories_table.php
+++ database/migrations/2023_11_20_154253_create_business_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('business_categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('category_name');
             $table->timestamps();
             $table->softDeletes();
