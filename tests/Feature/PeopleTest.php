55e8feff3444b51bcaf3aebd22be9f4f81a91f27
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-09 13:52:44 +0200

    Test 'people' pagefor empty and non empty

---
+++ tests/Feature/PeopleTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Person;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PeopleTest extends TestCase
+{
+    public function test_people_page_contains_empty_table(): void
+    {
+        // Assuming you have a User model and want to simulate a logged-in user
+        $user = User::factory()->create();
+
+        // Acting as the authenticated user
+        $this->actingAs($user);
+        $response = $this->get('/person');
+
+        $response->assertStatus(200);
+        $response->assertSee(__('No people found'));
+    }
+
+    public function test_people_page_contains_non_empty_table(): void
+    {
+        // Assuming you have a User model and want to simulate a logged-in user
+        $user = User::factory()->create();
+
+        Person::create([
+            'user_id' => $user->id,
+            'firstname' => 'Maria',
+            'lastname' => 'Lovecraft',
+        ]);
+        // Acting as the authenticated user
+        $this->actingAs($user);
+        $response = $this->get('/person');
+
+        $response->assertStatus(200);
+        $response->assertDontSee(__('No people found'));
+    }
+}
