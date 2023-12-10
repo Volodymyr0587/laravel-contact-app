9e99da526e9bf41532279f74e63f20693989b25b
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-10 21:42:42 +0200

    Remove (comment out) SoftDeletes from BusinessCategory model

--- app/Models/BusinessCategory.php
+++ app/Models/BusinessCategory.php
@@ -4,14 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class BusinessCategory extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory; //, SoftDeletes;
 
     public $fillable = ['user_id', 'category_name'];
 
