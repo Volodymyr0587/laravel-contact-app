cb8d4b131243047363ec19827a2d8b8d1cecb182
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-11 19:45:49 +0200

    Modify language switcher link to point directly to the /language route to maintain the current page's context (append the current URL's query parameters to the language switcher link)

--- resources/views/partials/language_switcher.blade.php
+++ resources/views/partials/language_switcher.blade.php
@@ -3,7 +3,10 @@
         @if($available_locale === $current_locale)
             <span class="bg-yellow-300 p-1 rounded-full ml-2 mr-2 text-gray-700 dark:text-gray-600">{{ $locale_name }}</span>
         @else
-            <a class="underline ml-2 mr-2 p-1 text-gray-700 dark:text-white" href="language/{{ $available_locale }}">
+            {{-- <a class="underline ml-2 mr-2 p-1 text-gray-700 dark:text-white" href="language/{{ $available_locale }}">
+                <span>{{ $locale_name }}</span>
+            </a> --}}
+            <a class="underline ml-2 mr-2 p-1 text-gray-700 dark:text-white" href="{{ url('language/' . $available_locale . '?' . http_build_query(request()->except('language'))) }}">
                 <span>{{ $locale_name }}</span>
             </a>
         @endif
