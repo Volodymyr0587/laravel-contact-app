94acb537b82f2600d27c31ebb80950a6bdb251dc
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-12 20:43:03 +0200

    Responsive navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -152,6 +152,80 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link>
+
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150
+                        dark:bg-gray-800 dark:text-gray-500 dark:hover:text-white">
+                            <div>{{  __('Contacts') }}</div>
+
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+
+                        <x-dropdown-link :href="route('person.index')">
+                            {{ __('People') }}
+                        </x-dropdown-link>
+
+                        <x-dropdown-link :href="route('business.index')">
+                            {{ __('Businesses') }}
+                        </x-dropdown-link>
+                    </x-slot>
+                </x-dropdown>
+
+            {{-- </x-responsive-nav-link>
+
+            <x-responsive-nav-link> --}}
+
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150
+                        dark:bg-gray-800 dark:text-gray-500 dark:hover:text-white">
+                            <div>{{  __('Manage contacts') }}</div>
+
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+
+                        <x-dropdown-link :href="route('businessCategory.index')">
+                            {{ __('Business categories') }}
+                        </x-dropdown-link>
+
+                        <x-dropdown-link :href="route('task.index')">
+                            {{ __('Tasks') }}
+                        </x-dropdown-link>
+
+                        <x-dropdown-link :href="route('tag.index')">
+                            {{ __('Tags') }}
+                        </x-dropdown-link>
+                    </x-slot>
+                </x-dropdown>
+
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link>
+                <x-nav-link :href="route('note.index')" :active="request()->routeIs('note.index')">
+                    {{ __('Notes') }}
+                </x-nav-link>
+
+                <x-nav-link :href="route('gallery.index')" :active="request()->routeIs('gallery.index')">
+                    {{ __('Gallery') }}
+                </x-nav-link>
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
