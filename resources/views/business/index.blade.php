058a8b11d74d04857c0a260fe355fa71464f26ca
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-12 16:58:01 +0200

    Action button component. Change buttons at businesses CRUD

--- resources/views/business/index.blade.php
+++ resources/views/business/index.blade.php
@@ -18,8 +18,9 @@
                 <div class="p-6 text-gray-900">
 
                     <div class="flex items-center justify-end">
-                        <a class="bg-blue-600 text-white py-2 px-3 rounded-full hover:bg-yellow-300 hover:text-blue-600"
-                            href="{{ route('business.create') }}">{{ __("Add Business") }}</a>
+                        <x-action-button>
+                            <a href="{{ route('business.create') }}">{{ __("Add Business") }}</a>
+                        </x-action-button>
                     </div>
 
                     <table class="table-fixed border-separate border-spacing-6">
