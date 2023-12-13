84243d6ecbd444e513aaebb9a6ea2fb82c829413
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-13 22:27:45 +0200

    Dark mode for login, register, forgot password pages

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm dark:bg-dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}>
