1879005ee0d302257298dad53bdaa27aee37655a
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-17 16:17:29 +0200

    Language switcher style colors

--- resources/views/partials/language_switcher.blade.php
+++ resources/views/partials/language_switcher.blade.php
@@ -1,11 +1,12 @@
 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
     @foreach($available_locales as $locale_name => $available_locale)
         @if($available_locale === $current_locale)
-            <span class="bg-yellow-300 p-1 rounded-md ml-1 text-gray-700 dark:text-gray-600">{{ $locale_name }}</span>
+            @if ($available_locale == 'ua')
+                <span class="bg-gradient-to-b from-blue-700 to-yellow-400 p-1 rounded-md ml-1 text-white">{{ $locale_name }}</span>
+            @elseif ($available_locale == 'en')
+                <span class="bg-gradient-to-br from-blue-500  to-red-400 p-1 rounded-md ml-1 text-white">{{ $locale_name }}</span>
+            @endif
         @else
-            {{-- <a class="underline ml-2 mr-2 p-1 text-gray-700 dark:text-white" href="language/{{ $available_locale }}">
-                <span>{{ $locale_name }}</span>
-            </a> --}}
             <a class="underline ml-1  p-1 text-gray-700 dark:text-white" href="{{ url('language/' . $available_locale . '?' . http_build_query(request()->except('language'))) }}">
                 <span>{{ $locale_name }}</span>
             </a>
