c44183c9e4bbd7f7b2ffab604603aea84d0ac732
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-17 16:40:55 +0200

    fix person image name after update; display person image on index page

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -48,6 +48,12 @@
                     <table class="table-fixed border-separate border-spacing-6">
                         <thead>
                             <x-table-row>
+                                <th>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                                    </svg>
+                                </th>
                                 <th>{{ __("Name") }}</th>
                                 <th>{{ __("Email") }}</th>
                                 <th>{{ __("Phone") }}</th>
@@ -59,6 +65,19 @@
                         <tbody>
                             @forelse ($people as $person)
                                 <x-table-row>
+                                    <td>
+                                        <span>
+                                            @if (!empty($person->image))
+                                                <img class="object-contain object-left h-10 w-10"
+                                                src="{{ is_url($person->image) ? $person->image : Storage::url($person->image) }}"
+                                                alt="Photo">
+                                            @else
+                                                {{-- Use default image --}}
+                                                <img class="object-contain object-left h-10 w-10"
+                                                src="{{ asset('images/person-no-image.png') }}" alt="Default photo">
+                                            @endif
+                                        </span>
+                                    </td>
                                     <td><a href="{{ route('person.show', $person->id) }}"
                                             class="text-blue-700 dark:text-blue-500 font-bold hover:bg-yellow-300 py-2 px-2 rounded-full">
                                             {{ $person->firstname }} {{ $person->lastname }}</a></td>
