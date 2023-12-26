d20f223b833ff8af337fd0dae72feeb6a10ba8c2
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-26 18:43:39 +0200

    tinycolor2 (change text color of tag accordind to bg-color)

--- resources/js/color-picker.js
+++ resources/js/color-picker.js
@@ -1,7 +1,10 @@
+import tinycolor from "tinycolor2";
+
 const colorPicker = document.getElementById("nativeColorPicker1");
 const changeColorBtn = document.getElementById("burronNativeColor");
 
 changeColorBtn.style.backgroundColor = colorPicker.value;
 colorPicker.addEventListener("input", () => {
   changeColorBtn.style.backgroundColor = colorPicker.value;
+  changeColorBtn.style.color = tinycolor(colorPicker.value).brighten(30).toHexString();
 });
