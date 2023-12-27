d58cf9e5ddcaf1ffd98af2f6022f33cbabf49688
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-27 19:21:02 +0200

    grid for search pages

--- resources/views/note/search.blade.php
+++ resources/views/note/search.blade.php
@@ -17,7 +17,7 @@
                 <div class="p-6 text-gray-900">
 
                     @if ($notes->isNotEmpty())
-                        <table class="table-fixed border-separate border-spacing-6 dark:text-white">
+                        <table class="table-fixed border-separate border-spacing-6 dark:text-white hidden md:block">
                             <thead>
                                 <tr>
                                     <th>{{ __("Title") }}</th>
@@ -56,6 +56,52 @@
                                 @endforeach
                             </tbody>
                         </table>
+
+                        <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4 mb-4 md:hidden">
+                            @forelse ($notes as $note)
+                            <div class="bg-white space-y-3 p-4 rounded-lg shadow-orange-300">
+                                <div class="flex items-center space-x-2 text-sm">
+                                    <div>
+                                        <x-buttons.show-button href="{{ route('note.show', $note->id) }}">
+                                            {{ $note->title }}
+                                        </x-buttons.show-button>
+                                    </div>
+                                    <div>
+                                        @foreach ($note->tags as $tag)
+                                            <span class="bg-green-500 py-1 px-1 rounded-full">
+                                                <a
+                                                    href="{{ route('note.getByTag', $tag->tag_name) }}">{{ $tag->tag_name }}</a>
+                                            </span>
+                                        @endforeach
+                                    </div>
+                                    <div>
+                                        <a href="{{ route('note.edit', $note->id) }}"
+                                            class="flex justify-center items-center">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                class="w-6 h-6 hover:text-green-500">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                            </svg>
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </div>
+                            @empty
+
+                            <div class="bg-white space-y-3 p-4 rounded-lg shadow-orange-300">
+                                <div class="flex items-center space-x-2 text-sm">
+                                    <div>
+
+                                        {{ __('No results') }}
+
+                                    </div>
+                                </div>
+                            </div>
+                            @endforelse
+                        </div>
+
                         {{ $notes->links() }}
                     @else
                         <div>
